1b31817b56add0009b50b62fcd57206634633fb9
Bhawna Shukla <shuklabhawna81@gmail.com>
2025-03-12 10:13:01 +0530

    variable in javascript

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+//DATA TYPES
+//two types of Data types
+// 1.primitive
+// /2.non-Primitive
+
+/**---PRIMITIVE DATA TYPE
+ * 1.simple and immutable 
+ * example-its not changed
+ * 2. 8 data type
+ */
+
+/**--NON-PRIMITIVE DATA TYPE
+ * 1.complex and mutable
+ * example- its changeable
+ * 2.data type
+ * Objectss
+ */
---
+++ variable.js
@@ -0,0 +1,36 @@
+// WELCOME JAVASCRIPT 
+
+//What is Variable ---->Varibale is place to stare data
+console.log("hello backend")
+console.log("------------------")
+
+// ONLY DECLARATION 
+var num
+console.log(num)
+console.log("-------------")
+
+
+// ONLY INITIALIZE
+var age = 19
+console.log(age)
+console.log("----------------")
+
+
+/**NAME A VARIABLE
+ * ---------RULE 1----------
+ * A-Z | a-z |0-9 | _ | $ (Dont use other character in a variable)
+ * ------KEYWORDS ---
+ * 63 keyword used (if,ifels,for,loop..........etc)[use should not used naming in variable because it is only for keywords]
+ *----RULE 2----
+ No space  between variable words. Eg-firstName (valid)
+                                     First Name (no valid) 
+   -----------GUIDELINES/SUGGESTIONS-------
+   it should meaning full declared name 
+
+   -------OBSERVATION------
+   they are case sensitive..eg-> bhawna,Bhawna,bHawna .
+
+ */
+// ----SINGLE LINE COMMENT---
+
+/**---MULTI LINE COMMENT */
