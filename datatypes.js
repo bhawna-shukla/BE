143d15d20134622dc53026353b54a7d5b1f8f9a3
Bhawna Shukla <shuklabhawna81@gmail.com>
2025-03-13 16:33:18 +0530

    8 Data types

--- datatypes.js
+++ datatypes.js
@@ -22,4 +22,51 @@ var name = "Bhawna"
 var title = "Shukla"
 console.log(name)
 console.log(title)
+console.log(typeof(name))
+
+console.log("--------------")
+//number
+var age=20
+console.log("age =",age)
+console.log(typeof(age))
+ console.log("----------")
+//boolean
+var x = true;
+var y = false;
+console.log(x)
+console.log(y)
+console.log(typeof(x))
+console.log("---------")
+
+//BigInt
+var num=2n
+console.log(typeof(num))
+console.log("--------------------")
+
+//undefind
+var a
+console.log(a)
+
+//null
+var b = null
+console.log(b)
+console.log(typeof(b))
+
+// symbole
+var sym3 = Symbol("foo");
+console.log(typeof(sym3))
+console.log("------------")
+
+
+
+/**---NON- PRIMITIVE DATA TYPE--- */
+// OBJECT
+var stu_id={
+    name : "Khushboo",
+    course : "BCA",
+    hobby : "problem-solving"
+
+}
+ console.log(stu_id)
+ console.log("-------------")
 
